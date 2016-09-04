c7c420138f9d5fefcbda762a2daf112585e0c3c9
ajaxboy <cjxxi21@gmail.com>
2016-09-04 07:27:37 -0700

    ==POR Game For MAC == Game can now run on Mac. This file will let you run the game. Ability to run the game through symlinks. Removed TLOPO Crap. Update main module execution path, for python.

--- darwin/start_game_localhost.sh
+++ darwin/start_game_localhost.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-cd ..
+
+###
+# Check if it is a symlink. If it is, then sets the correct path so python won't complain.
+###
+if [[ -L "${0%/*}" ]]; then
+    cd `readlink "${0%/*}"  | xargs dirname | xargs dirname`
+else
+    cd ..
+fi
+
 
 export DYLD_LIBRARY_PATH=`pwd`/Libraries.bundle
 export DYLD_FRAMEWORK_PATH="Frameworks"
@@ -7,8 +16,8 @@ export DYLD_FRAMEWORK_PATH="Frameworks"
 read -p "Username: " LOGIN_COOKIE
 read -p "Gameserver: " LOGIN_SERVER
 
-export TLOPO_PLAYCOOKIE=$LOGIN_COOKIE
-export TLOPO_GAMESERVER=$LOGIN_SERVER
+export POR_PLAYCOOKIE=$LOGIN_COOKIE
+export POR_GAMESERVER=$LOGIN_SERVER
 
 echo "=============================="
 echo "Starting Pirates Online..."
@@ -16,4 +25,4 @@ echo "Username: $LOGIN_COOKIE"
 echo "Gameserver: $LOGIN_SERVER"
 echo "=============================="
 
-ppython -m main
+ppython -m pirates.piratesbase.PiratesStartDev
