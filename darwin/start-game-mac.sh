b612ced84bf6d2d20b678f8b35ae283d4b863959
ajaxboy <cjxxi21@gmail.com>
2016-09-05 12:45:46 -0700

    mac server

--- darwin/start-game-mac.sh
+++ darwin/start-game-mac.sh
@@ -9,16 +9,24 @@ else
     cd ..
 fi
 
-#read -p "Username: " LOGIN_COOKIE
-#read -p "Gameserver: " LOGIN_SERVER
 
-export POR_PLAYCOOKIE=cj
-export POR_GAMESERVER=104.251.215.54
+if [[  whoami -eq 'easywin123' ]]; then
+    export POR_PLAYCOOKIE=cj
+    export POR_GAMESERVER=127.0.0.1
+else
+
+    read -p "Username: " LOGIN_COOKIE
+    read -p "Gameserver: " LOGIN_SERVER
+
+    export POR_PLAYCOOKIE=$LOGIN_COOKIE
+    export POR_GAMESERVER=$LOGIN_SERVER
+fi
+
 
 echo "=============================="
 echo "Starting Pirates Online..."
 echo "Username: $LOGIN_COOKIE"
-echo "Gameserver: $LOGIN_SERVER"
+echo "Gameserver: $POR_GAMESERVER"
 echo "=============================="
 
 ppython -m pirates.piratesbase.PiratesStartDev
