f13a2877f8c02c28342f5618445cef6b56295093
ajaxboy <cjxxi21@gmail.com>
2016-09-05 14:02:46 -0700

    update Astron to detech linux/darwin.

--- astron/linux/processes/astron.sh
+++ astron/linux/processes/astron.sh
@@ -5,8 +5,23 @@ kill -9 `pgrep -f "astron"`
 
 cd ..
 
+    #
+    #  Need to Update the .yml config file to use mongodb!!!!!!!!!
+    #
+
 #start ai server
-astron/astrondmac2 --loglevel info astron/config/cluster_yaml.yml  > logs/astron.log 2>&1 &
+if [ "$(uname)" == "Darwin" ]; then
+    #Mac OS X platform
+    astron/astrondmac2 --loglevel info astron/config/cluster_yaml.yml  > logs/astron.log 2>&1 &
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    #GNU/Linux platform
+    astron/astrondlinux --loglevel info astron/config/cluster_yaml.yml  > logs/astron.log 2>&1 &
+
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    #Windows NT platform
+    #We don't run this on Windows.....
+fi
 
 echo "Astron - Process ID#" `pgrep -f "astron"`
 echo "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
