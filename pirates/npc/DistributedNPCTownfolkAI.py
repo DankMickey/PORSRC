2067fe88843df10d348ada70e3e2ca9ee9fb7c20
Tohka <darktohka@yandex.com>
2016-10-22 20:12:15 +0300

    npc: Fixed NPCs spawning in walls

--- pirates/npc/DistributedNPCTownfolkAI.py
+++ pirates/npc/DistributedNPCTownfolkAI.py
@@ -183,6 +183,7 @@ class DistributedNPCTownfolkAI(DistributedBattleNPCAI, DistributedShopKeeperAI):
         if cls is None:
             cls = DistributedNPCTownfolkAI
         avType = getattr(AvatarTypes, data['Category'])
+        data['GridPos'] = None
         obj = DistributedBattleNPCAI.makeFromObjectKey(cls, spawner, uid, avType, data)
 
         helpId = data.get('HelpID', 'NONE')
