274434652e9f9a13b843790fab0597eb5d8e76f0
Sean Oberoi <seano@Seans-MacBook-Air.local>
2016-10-24 12:13:58 -0700

    Changed from access 100 up to 700 up for GM Chat

--- pirates/pirate/DistributedPlayerPirate.py
+++ pirates/pirate/DistributedPlayerPirate.py
@@ -3567,7 +3567,7 @@ class MinimapPlayerPirate(MinimapBattleAvatar):
     def setAdminAccess(self, access):
         self.adminAccess = access
         if self.isLocal():
-            self.cr.wantMagicWords = self.adminAccess >= MINIMUM_MAGICWORD_ACCESS
+            self.cr.wantMagicWords = self.adminAccess >= CATEGORY_GAME_MASTER.access
     
     def requestEmote(self, emoteId):
         gamestate = localAvatar.getGameState()
