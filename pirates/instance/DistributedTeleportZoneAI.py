347d0305f70ee1f2a6fd98cf158dcd3e2d1548d2
Tohka <darktohka@yandex.com>
2016-10-28 10:51:06 +0300

    cleanup: Remove DistributedTeleportZone/AI

--- pirates/instance/DistributedTeleportZoneAI.py
+++
@@ -1,5 +0,0 @@
-from pirates.instance import DistributedInstanceBaseAI
-
-class DistributedTeleportZoneAI(DistributedInstanceBaseAI.DistributedInstanceBaseAI):
-    def __init__(self, air):
-        DistributedInstanceBaseAI.DistributedInstanceBaseAI.__init__(self, air)
