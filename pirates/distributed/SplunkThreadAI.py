42759b6f8bb2a4c18dc5d4caba2666c5038a5e59
Tohka <darktohka@yandex.com>
2016-11-13 11:32:41 +0200

    logging: Added log try catch

--- pirates/distributed/SplunkThreadAI.py
+++ pirates/distributed/SplunkThreadAI.py
@@ -1,9 +1,12 @@
 from direct.distributed.AstronInternalRepository import msgpack_encode
 from direct.distributed.PyDatagram import PyDatagram
+from direct.directnotify.DirectNotifyGlobal import directNotify
 from threading import Thread
+import traceback
 
 class SplunkThreadAI(Thread):
-
+    notify = directNotify.newCategory('SplunkThreadAI')
+    
     def run(self):
         while True:
             item = simbase.air.logQueue.get()
@@ -14,7 +17,11 @@ class SplunkThreadAI(Thread):
                     msgpack_encode(dg, item)
                     simbase.air.eventSocket.Send(dg.getMessage())
             else:
-                del item['type']
-                simbase.air.analyticsMgr.track(logType, item)
+                logType = item.pop('type')
+                
+                try:
+                    simbase.air.analyticsMgr.track(logType, item)
+                except Exception as e:
+                    self.notify.warning('Exception happened while sending log to Splunk:\n%s' % traceback.format_exc())
 
             simbase.air.logQueue.task_done()
