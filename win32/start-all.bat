77baf82240f2114a8eed2768b23229c6f26ce26d
Nick Renieris <makren67@gmail.com>
2017-02-16 04:00:16 +0200

    Fix start-all script
    Fix path for administrator accounts
    Fix bug with MongoDB by delaying the initialization of everything else

--- win32/start-all.bat
+++ win32/start-all.bat
@@ -1,8 +1,12 @@
 @echo off
 title Starting POR...
 
+set p=%~dp0        :: sets 'p' to drive and path of batch file
+cd %p%            :: changes directory
+
 start start-mongo-server.bat
+ping 127.0.0.1 -n 4 > nul
 start start-astron-cluster.bat
 start start-uberdog-server.bat
 start start-ai-server.bat
-start start-game.bat
+start start-clients.bat
